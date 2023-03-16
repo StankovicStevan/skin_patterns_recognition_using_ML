0bf0e28896b5419fd9c5cb5b748fa4ce947d998b
Stevan Stankovic <stankovicstevan98@gmail.com>
2023-03-16 21:58:18 +0100

    Checks returned

--- data_engineering/loading_input_file.py
+++ data_engineering/loading_input_file.py
@@ -11,7 +11,10 @@ file_name = 'HAM10000_metadata.csv'
 file_path = '../data/input/'
 dataset_dir = file_path + file_name
 
-
+# Checking whether file already is downloaded
+if os.path.exists(dataset_dir):
+    print("Found dataset directory, exiting")
+    exit(0)
 
 print("Dataset not found, using kaggle-api tool for download")
 
